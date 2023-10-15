1ed72bb5d7f9479d21fc648315b267499bd98cae
diana <dshevchenko22@fpcoruna.afundacion.org>
2023-10-15 17:20:27 +0200

    Ejercicio 2 del 1.2 corregido (me lie con los ejercicios)

--- sprint1apache/www/site3/example2.php
+++ sprint1apache/www/site3/example2.php
@@ -1,9 +1,12 @@
 <html>
 	<body>
-		<h1>El ganador es...</h1>
-		<?php
-		  $nombre = 'Milhouse';
-		  echo $nombre;		
-		?>
+		<h1>Página de bienvenidas</h1>
+		   <?php
+			function dar_bienvenida($nombre) {
+			   echo "¡Bienvenido/a, " . $nombre . "!";
+			}
+
+			dar_bienvenida("Milhouse");
+		   ?>
 	</body>
 </html>
