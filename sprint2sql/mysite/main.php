6bc1b084af014a24a2f11434f5771b80035fdd52
diana <dshevchenko22@fpcoruna.afundacion.org>
2023-10-23 13:42:33 +0200

    2.2 Solo se muestra bien una imagen

--- sprint2sql/mysite/main.php
+++ sprint2sql/mysite/main.php
@@ -1,4 +1,27 @@
 <?php
 	$db = mysqli_connect('localhost', 'root', '1234','mysitedb') or die ('Fail');
 ?>
-<h1>Conexión establecida</h1>
+<html>
+	<body>
+		<h1>Conexión establecida</h1>
+
+		<?php
+			//lanzar un query
+			$query = 'select * from tCanciones';
+			$result = mysqli_query ($db, $query) or die('Query error');
+
+			//recorrer el resultado
+			while ($row = mysqli_fetch_array($result)) {
+				echo $row['nombre'];
+				echo '<br>';
+				echo '<img src=' .$row[2]. '/>';
+				echo '<br>';
+				echo $row[3];
+				echo '<br>';
+				echo $row[4];
+				echo '<br>';
+			}
+			mysqli_close($db);
+		?>
+	</body>
+</html>
