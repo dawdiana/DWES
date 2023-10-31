3badecc99324078fd61f0c23b6dce6e17c0d8c7e
dawdiana <145001704+dawdiana@users.noreply.github.com>
2023-10-31 13:43:00 +0100

    Add files via upload
    Ejercicio 2 del 2.3

--- sprint2Python/adivinanza.py
+++ sprint2Python/adivinanza.py
@@ -1,26 +1,64 @@
+if __name__ == '__main__': 
 
-print("Adivina adivinanza:")
-print("Oro parece, plata no es.")
-print("")
-print("a. Es L")
-print("b. Es el amor de mi vida")
-print("c. Es un plátano")
-print("")
-print("¿Qué es?")
-print("")
-respuesta = input()
+#LISTA DE DICCIONARIOS
+   
+ lista_diccionarios = [  
+    {
+        "Adivinanza": "Oro parece, plata no es.",
+        "Opciones": 
+        """a. Es L
+b. Es el amor de mi vida
+c. Es un plátano""",
+        "Correcto" : "c"
+    },
 
-#Mirar que solo acepte respuestas como a, b o c
-while respuesta !="c" and respuesta !="a" and respuesta !="b":
-    print("")
-    print ("Aprende a leer y contesta bien >:(")
-    print("¿Qué es?") 
-    print("")
-    respuesta = input()
-    print("")
+    {
+        "Adivinanza": "La luna y el sol la llevan, pero en el aire no la ves, ¿Qué es?",
+        "Opciones": 
+        """a. Gravedad
+b. La letra L
+c. Una marioneta""",
+        "Correcto" : "b"
+    },
 
-if respuesta != "c":
-    print("Eres tonto?, era la c.")
-    print("")
-else:
-    print("Muy bien!!")
+    {
+        "Adivinanza": "Tengo agujas y no sé coser, tengo números y no sé leer.",
+        "Opciones": 
+        """a. Un pescado matemático 
+b. Un reloj
+c. Mis amigos""",
+
+        "Correcto": "b"
+    }
+]
+
+
+#Contador de puntos
+contador = 0
+
+for i in lista_diccionarios:
+        print("")
+        print("Adivina adivinanza:")
+        print(i.get("Adivinanza")) #invocar a una variable de un diccionario desde una lista
+        print("")
+        print(i.get("Opciones"))
+        respuesta=input("¿Qué es? ")
+        
+        if(respuesta.lower()!="a" and respuesta.lower()!="b" and respuesta.lower()!="c"):
+            while (respuesta.lower()!="a" and respuesta.lower()!="b" and respuesta.lower()!="c"): #pillar las respuestas en lowerCase
+                print("[Lee mejor las opciones y vuelve a contestar]")
+                respuesta=input("¿Qué es? ")
+
+        if (respuesta!=i.get("Correcto")):
+            contador = contador - 5
+            print("")
+            print("Era la " + i.get("Correcto") + " tont@ :P")
+        else:
+            contador = contador + 10
+            print("")
+            print("Muy bien!!")
+            print("")
+
+
+print("Tu puntuación es de ", contador, " puntos.") #si pongo + en vez de , se concatena 
+
